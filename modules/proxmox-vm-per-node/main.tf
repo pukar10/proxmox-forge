a582d9412f772da1b744be30632e5dba9cba3748
psubedi <psubedi@idtus.com>
2025-08-09 05:15:56 -0400

    user_data_file_id to one line

--- modules/proxmox-vm-per-node/main.tf
+++ modules/proxmox-vm-per-node/main.tf
@@ -66,9 +66,7 @@ resource "proxmox_virtual_environment_vm" "vm" {
     datastore_id = var.datastore_vm
 
     # attach user-data if present on this node
-    user_data_file_id = contains(keys(proxmox_virtual_environment_file.user_data), each.value.node_name)
-      ? proxmox_virtual_environment_file.user_data[each.value.node_name].id
-      : null
+    user_data_file_id = var.user_data_content != "" ? proxmox_virtual_environment_file.user_data[each.value.node_name].id : null
 
     user_account {
       username = var.ci_username
